e5bbdc724d02c38abba66d87b6915b41be014888
Lautaro Gonzalez Ruiz <lautawork@gmail.com>
2023-09-19 00:25:42 -0300

    Logica de mobilidad de player. Limite de mapa funcional.

--- Js/game.js
+++ Js/game.js
@@ -7,6 +7,10 @@ const btnDown = document.querySelector("#down");
 
 let canvasSize;
 let elementsSize;
+let playerPosition = {
+  x: undefined,
+  y: undefined,
+};
 
 const setCanvasSize = () => {
   if (window.innerHeight > window.innerWidth) {
@@ -30,7 +34,7 @@ const startGame = () => {
   game.font = elementsSize + "px Verdana";
   game.textAlign = "end";
 
-  const map = maps[2]; // Hacemos referencia a nuestro arreglo maps que contiene los distintos niveles.
+  const map = maps[0]; // Hacemos referencia a nuestro arreglo maps que contiene los distintos niveles.
   /*
   Filas del mapa.
   Limpiamos espacios con TRIM.
@@ -44,27 +48,63 @@ const startGame = () => {
   */
   const mapRowsCol = mapRows.map((row) => row.trim().split(""));
 
+  game.clearRect(0, 0, canvasSize, canvasSize);
+
   mapRowsCol.forEach((row, rowI) => {
     row.forEach((col, colI) => {
       const emoji = emojis[col];
       const posX = elementsSize * (colI + 1.3);
       const posY = elementsSize * (rowI + 0.95);
+
+      if (col == "O") {
+        if (!playerPosition.x && !playerPosition.y) {
+          playerPosition.x = posX;
+          playerPosition.y = posY;
+        }
+      }
       game.fillText(emoji, posX, posY);
     });
   });
+  movePlayer();
+};
+
+const movePlayer = () => {
+  game.fillText(emojis["PLAYER"], playerPosition.x, playerPosition.y);
 };
 
 const moveByKeys = (e) => {
-  if (e.key == "ArrowUp") moveUp;
-  else if (e.key == "ArrowLeft") moveLeft;
-  else if (e.key == "ArrowRight") moveRight;
-  else if (e.key == "ArrowDown") moveDown;
+  if (e.key == "ArrowUp") moveUp();
+  else if (e.key == "ArrowLeft") moveLeft();
+  else if (e.key == "ArrowRight") moveRight();
+  else if (e.key == "ArrowDown") moveDown();
 };
 
-const moveUp = () => {};
-const moveLeft = () => {};
-const moveRight = () => {};
-const moveDown = () => {};
+const moveUp = () => {
+  if (Math.floor(playerPosition.y) > elementsSize) {
+    playerPosition.y = playerPosition.y - elementsSize;
+    startGame();
+  }
+};
+const moveLeft = () => {
+  if (playerPosition.x < elementsSize * 2) {
+  } else {
+    playerPosition.x -= elementsSize;
+    startGame();
+  }
+};
+const moveRight = () => {
+  if (Math.ceil(playerPosition.x) < 10 * elementsSize) {
+    playerPosition.x = playerPosition.x + elementsSize;
+    startGame();
+  }
+};
+const moveDown = () => {
+  if (playerPosition.y > elementsSize * 9) {
+  } else {
+    playerPosition.y += elementsSize;
+    startGame();
+  }
+};
 
 window.addEventListener("load", setCanvasSize);
 window.addEventListener("resize", setCanvasSize);
